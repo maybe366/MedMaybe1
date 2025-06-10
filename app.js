6d8a1d7130b4e5dd58e7d91a9c85f19001e09d32
maybe366 <dmiitriieva3@gmail.com>
2025-06-11 02:32:28 +0800

    fix: Configure CORS for production

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(
+	cors({
+		origin: 'https://medmaybe.netlify.app', // Разрешаем запросы ТОЛЬКО с твоего сайта
+	})
+)
 app.use(express.json())
 
 
